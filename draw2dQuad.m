aeeb7b77a08b9ed8dcdb5e580d5803a3ad366298
Trexter <sheridak@purdue.edu>
2017-11-04 12:15:06 -0400

    the ekf is running

--- draw2dQuad.m
+++ draw2dQuad.m
@@ -1,4 +1,4 @@
-function draw2dQuad(pos_function, theta_function, synthetic_z_func, t_eval)
+function draw2dQuad(pos_function, theta_function, synthetic_z_func, mu, t_eval)
     
     syms t
 
@@ -26,6 +26,11 @@ function draw2dQuad(pos_function, theta_function, synthetic_z_func, t_eval)
     text(pos(1) -z(1)/2 * sin(theta), pos(2) -z(1)/2 * cos(theta), num2str(z(1), 'Range Sensor: %.2f'))
     
     text(-7.5, 0.5, num2str(t_eval, 'Time: %.2f'))
+    
+    
+    quiver(mu(1), mu(2), 0.5*cos(mu(3)), -0.5*sin(mu(3)));
+    quiver(mu(1), mu(2), 0.5*sin(mu(3)), 0.5*cos(mu(3)));
+    
     
     hold off
     
